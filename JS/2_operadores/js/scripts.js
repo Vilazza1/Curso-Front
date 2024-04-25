cf4b077bdbdf69e096ec5f52b0e0b8b785b66e89
Vilazza1 <vinicius.vilaca2023@gmail.com>
2024-04-25 14:29:51 -0300

    Seção de Tipo de daos e operadores incompleto

---
+++ JS/2_operadores/js/scripts.js
@@ -0,0 +1,35 @@
+// 1 - Number 
+
+console.log(typeof 2);
+console.log(typeof 5.15);
+console.log(typeof -127);
+
+// 2 - Ops. aritmérocas
+
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(10 / 2);
+
+console.log(5 + (4 * 2));
+
+// 3 - Special numbers
+
+console.log(typeof Infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12*"asd");
+
+console.log(typeof NaN);
+
+// 4 - String
+
+console.log( typeof "Um Texto");
+console.log( typeof `Mais um texto`);
+
+// 5 - Símbolos especiais em string
+
+console.log("Testa a \n quebra de linha")
+
+console.log("Espaçamento \t de tab")
